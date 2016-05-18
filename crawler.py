b1b0d4f70aaacc354f37e883f6f772ca495f97a2
James Lett <jlett89@gmail.com>
2016-05-18 14:46:12 -0700

    targeting specific info

--- crawler.py
+++ crawler.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.git("http://www.theatlantic.com/")
+url = "http://www.theatlantic.com/" # Only for learning purposes, please don't use data without approval
+r = requests.get(url)
 
 soup = BeautifulSoup(r.content)
+
+links = soup.find_all("a")
+
+for link in links:
+    if "http" in link.get("href"):
+        print "<a href='%s'>%s</a>" %(link.get("href"), link.text)
+
+
+g_data = soup.find_all("div",{"class": "content"})
